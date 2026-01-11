f73d2de31d2185bb72c6b2f9c5cb241d41e94f56
lorita444 <lorymereuta2006@gmail.com>
2026-01-11 21:56:24 +0200

    Add migration Postari

---
+++ Proiect_dubla1/Migrations/20260111195037_Postari.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proiect_dubla1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Postari : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
